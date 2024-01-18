a6de697a433ffc474242478f72ffc82985c968a7
hassanelfaqir <hassanelfaqir@Linux.myguest.virtualbox.org>
2024-01-18 11:32:30 +0100

    AirBnB_clone_v2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -11,7 +11,7 @@ env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_pack():
-    """Create a tar gzipped archive the directory web_static."""
+    """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
     file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
                                                          dt.month,
@@ -28,7 +28,7 @@ def do_pack():
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
+    """Distributes an archive a web server.
 
     Args:
         archive_path (str): The path of the archive to distribute.
